6c23ae279704aa027d3f3ede9502624a749bb945
Patricio Yu <yupatricio0@gmail.com>
2023-10-26 17:00:09 -0300

    clear the window each frame

--- core/src/com/sticksouls/StickSouls.java
+++ core/src/com/sticksouls/StickSouls.java
@@ -3,6 +3,7 @@ package com.sticksouls;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -32,6 +33,7 @@ public class StickSouls extends ApplicationAdapter {
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
+	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		
 		// check if the assets are already loaded
