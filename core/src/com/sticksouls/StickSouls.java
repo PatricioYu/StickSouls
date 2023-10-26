dcada71b5686143a2c32b6b7b8d71e6ff1da5689
Patricio Yu <yupatricio0@gmail.com>
2023-10-26 16:29:10 -0300

    Set up camera and implemented assetManager

--- core/src/com/sticksouls/StickSouls.java
+++ core/src/com/sticksouls/StickSouls.java
@@ -1,31 +1,50 @@
 package com.sticksouls;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class StickSouls extends ApplicationAdapter {
+	AssetManager assetManager;
 	SpriteBatch batch;
-	Texture img;
+	OrthographicCamera camera;	
 	
 	@Override
 	public void create () {
+		// Initialize the AssetManager for asset loading
+		assetManager = new AssetManager();
+
+		assetManager.load("badlogic.jpg", Texture.class);
+				
+		// Create a new OrthographicCamera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		// Set an initial zoom level
+		camera.zoom = 1.0f;
+		
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		
+		// check if the assets are already loaded
+		if(assetManager.update()) {
+			batch.draw(assetManager.get("badlogic.jpg", Texture.class), 0, 0);
+		}
+		
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
+		assetManager.dispose();
 		batch.dispose();
-		img.dispose();
 	}
 }
